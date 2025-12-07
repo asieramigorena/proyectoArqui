ebea3752509d25ea712fd74f85658be883ffbc41
Asier Amigorena <asier.amigorena@opendeusto.es>
2025-12-07 13:35:33 +0100

    Añade procesos

--- main.py
+++ main.py
@@ -2,27 +2,47 @@ import random
 
 from gpiozero import LED, Button
 import threading, time, queue
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from fastapi import FastAPI
 from datetime import datetime
 
-app = FastAPI()
+colaApi = Queue()
+pApi, pJuego = None, None
 
-
-button = Button(16, pull_up=False, bounce_time=0.05)
-ledr = LED(17)
-ledv = LED(27)
 eventos = queue.Queue()
-R, V, A, N = range(4)
-estado = N
+r, v, a, n = range(4)
+estado = n
 puntuacion = 1
-puntuaciones = []
-combinaciones = []
-respuesta = []
+puntuaciones = [[], []]
 
-def inicio():
-    for i in range(10):
-        combinaciones[i] = random.randint(0, 1)
+
+def juego():
+    button = Button(16, pull_up=False, bounce_time=0.05)
+    ledr = LED(17)
+    ledv = LED(27)
+    otra_vez = True
+    while otra_vez:
+        combinaciones = []
+        for i in range(10):
+            combinaciones[i] = random.randint(0, 1)
+
+        t_seleccion = threading.Thread(target=seleccion, args=(button,), daemon=True)
+        t_comprobacion = threading.Thread(target=comprobacion, args=combinaciones, daemon=True)
+        t_logica_led = threading.Thread(target=logica_led, args=(ledr, ledv), daemon=True)
+
+        t_seleccion.start()
+        t_logica_led.start()
+        t_comprobacion.start()
+
+        t_logica_led.join()
+        t_seleccion.join()
+        t_comprobacion.join()
+
+        ledr.off()
+        ledv.off()
+        ledr.close()
+        ledv.close()
+
 
 def seleccion(button):
     while True:
@@ -30,11 +50,11 @@ def seleccion(button):
         time1 = time.time()
         button.wait_for_release()
         time2 = time.time()
-        #Larga = Verde // Corta = Roja
+        # Larga = Verde // Corta = Roja
         eventos.put(0) if (time2 - time1) < 2 else eventos.put(1)
 
-def comprobacion():
-    global respuesta
+
+def comprobacion(combinaciones):
     global puntuacion
     running = True
     while running:
@@ -43,17 +63,15 @@ def comprobacion():
             if actual != combinaciones[i]:
                 running = False
                 break
-            else:
-                respuesta.append(actual)
         puntuacion += 1
+        if puntuacion == 10:
+            print("Has ganado")
+            running = False
+    puntuaciones[1].append(puntuacion)
+    puntuaciones[0].append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
-def juego():
-    t_seleccion = threading.Thread(target=seleccion, args=(button,))
-    t_logicaled = threading.Thread(target=logica_led, args=())
-    t_seleccion.start(); t_logicaled.start()
-    t_logicaled.join(); t_seleccion.join()
 
-def logica_led():
+def logica_led(ledr, ledv):
     global estado
     while True:
         match estado:
@@ -72,11 +90,26 @@ def logica_led():
             case _:
                 print("Error en led_inicio")
 
-if __name__ == '__main__':
+
+def servidor():
+    global puntuacion
+    app = FastAPI()
+    while True:
+        print("test")
+        time.sleep(1)
+
+
+def main():
+    global p_api, p_juego
     try:
         while True:
-            inicio()
-
+            p_api = Process(target=servidor)
+            p_juego = Process(target=juego)
 
     except KeyboardInterrupt:
         print("\nTerminando ejecución, adios")
+        p_api.join()
+        p_juego.join()
+
+if __name__ == '__main__':
+    main()
