8f7d93a6cb50d2d90b91cabf3305396cca64e5a5
Asier Amigorena <asier.amigorena@opendeusto.es>
2025-12-09 11:34:10 +0100

    Arregla errores

--- main.py
+++ main.py
@@ -26,17 +26,27 @@ def juego():
         for i in range(10):
             combinaciones[i] = random.randint(0, 1)
 
-        t_seleccion = threading.Thread(target=seleccion, args=(button,), daemon=True)
-        t_comprobacion = threading.Thread(target=comprobacion, args=combinaciones, daemon=True)
-        t_logica_led = threading.Thread(target=logica_led, args=(ledr, ledv), daemon=True)
+        evento = threading.Event()
+
+        t_seleccion = threading.Thread(target=seleccion, args=(button, evento), daemon=True)
+        t_comprobacion = threading.Thread(target=comprobacion, args=(combinaciones,), daemon=True)
+        t_logica_led = threading.Thread(target=logica_led, args=(ledr, ledv, evento), daemon=True)
 
         t_seleccion.start()
         t_logica_led.start()
         t_comprobacion.start()
 
+        t_comprobacion.join()
+
+        try:
+            ultimo = eventos.get()
+            otra_vez = True if ultimo == 1 else False
+        except queue.Empty:
+            otra_vez = False
+
+        evento.set()
         t_logica_led.join()
         t_seleccion.join()
-        t_comprobacion.join()
 
         ledr.off()
         ledv.off()
@@ -44,8 +54,8 @@ def juego():
         ledv.close()
 
 
-def seleccion(button):
-    while True:
+def seleccion(button, evento):
+    while not evento.is_set():
         button.wait_for_press()
         time1 = time.time()
         button.wait_for_release()
@@ -55,25 +65,35 @@ def seleccion(button):
 
 
 def comprobacion(combinaciones):
-    global puntuacion
+    global puntuacion, estado
     running = True
     while running:
+        for i in range(puntuacion):
+            if combinaciones[i] == 0:
+                estado = r
+                time.sleep(0.3)
+            else:
+                estado = v
+                time.sleep(0.3)
+            estado = n
+            time.sleep(0.3)
         for i in range(puntuacion):
             actual = eventos.get()
             if actual != combinaciones[i]:
                 running = False
                 break
-        puntuacion += 1
-        if puntuacion == 10:
-            print("Has ganado")
-            running = False
+        if running:
+            puntuacion += 1
+            if puntuacion == 10:
+                print("Has ganado")
+                running = False
     puntuaciones[1].append(puntuacion)
     puntuaciones[0].append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
 
-def logica_led(ledr, ledv):
+def logica_led(ledr, ledv, evento):
     global estado
-    while True:
+    while not evento.is_set():
         match estado:
             case 0:
                 ledr.on()
