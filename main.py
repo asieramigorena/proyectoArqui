5f841e8887063feb34401b4aa538c7caf764bf24
Gonzalo Portilla <127732164+gonzpm@users.noreply.github.com>
2025-12-09 22:58:01 +0100

    revertir cambios

--- main.py
+++ main.py
@@ -44,22 +44,20 @@ def juego(cola_puntuacion):
 
             t_comprobacion.join()
 
-            # Primero paramos la seleccion para que no meta pulsaciones donde no toca
-            evento.set()
-            t_seleccion.join()
-            t_logica_led.join()    
-
-            # Reiniciar el juego safe
-            estado = a
-            semaforo_in.release()
-
             try:
-                ultimo = eventos.get(timeout=0.1)
-                otra_vez = (ultimo == 0)
+                estado = a
+                semaforo_in.release()
+                ultimo = eventos.get()
+                otra_vez = True if ultimo == 0 else False
+                estado = n
             except queue.Empty:
                 otra_vez = False
+                
+            evento.set()
+            t_logica_led.join()
+            semaforo_in.release()
+            t_seleccion.join()
 
-            estado = n
 
     finally:
         ledr.off()
